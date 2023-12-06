176e4995295dff747f381bed1f6a0b770ba15200
MatiasV4squez <135163669+MatiasV4squez@users.noreply.github.com>
2023-12-06 10:45:36 -0300

    Delete JS directory

--- JS/contacto.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("miFormulario").addEventListener("submit", function (event) {
-    event.preventDefault(); 
-    var mensaje = document.getElementById("mensaje");
-    mensaje.style.display = "block";
-});
